1bebf958f31f4bc949d476c696a8c5c2397472de
Steven Spencer <steve35spencer@gmail.com>
2020-06-10 19:02:54 -0400

    day 3 mvp

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,64 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        #1 Check if there is no root
+            # if there isn't, create the node and park it
+        if self is None:
+            self = BSTNode(value)
+        #2 Otherwise, there is a root
+            # compare the vlaue to the root's value to determine which direction to go in
+            # if the value is less than the root's value, go left, 
+            # if the vaue is greater or equal to the root's value, go right
+        else:
+            if value < self.value:
+                if self.left is None:
+                    self.left = BSTNode(value)
+                else:
+                    self.left.insert(value)
+            else:
+                if self.right is None:
+                    self.right = BSTNode(value)
+                else:
+                    self.right.insert(value)
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        output = False
+        if self is None:
+            output = False
+        else:
+            if self.value == target:
+                output = True
+            else:
+                if target < self.value:
+                    if self.left is None:
+                        output = False
+                    else:
+                        output = self.left.contains(target)
+                else:
+                    if self.right is None:
+                        output = False
+                    else:
+                        output = self.right.contains(target)
+        return output
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        current = self
+        while(current.right):
+            current = current.right
+        return current.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        if self:
+            fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
